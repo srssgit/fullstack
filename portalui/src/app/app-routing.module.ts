d58557dffe51fa7ddfea77e89e4269ef657e3c6d
srssgit <67203399+srssgit@users.noreply.github.com>
2020-06-21 13:10:51 +0530

    Add files via upload

--- portalui/src/app/app-routing.module.ts
+++ portalui/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortalComponent } from './portal/portal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'portal', component: PortalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
